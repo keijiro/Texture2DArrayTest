7b6d4f655bc04638aa1aba64d9e7ec51845f5b62
Keijiro Takahashi <keijiro@Ultra.jt.unity3d.com>
2017-07-04 17:13:55 +0900

    Initial commit.

---
+++ Assets/Reader.shader
@@ -0,0 +1,31 @@
+﻿Shader "Hidden/Reader"
+{
+    Properties
+    {
+        _BufferTex("", 2DArray) = "" {}
+    }
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex vert_img
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            UNITY_DECLARE_TEX2DARRAY(_BufferTex);
+
+            fixed4 frag(v2f_img i) : SV_Target
+            {
+                float3 uvw = float3(i.uv, frac(_Time.y) * 8);
+                float br = UNITY_SAMPLE_TEX2DARRAY(_BufferTex, uvw).r;
+                return br;
+            }
+
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Tester.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Tester : MonoBehaviour
+{
+    [SerializeField, HideInInspector] ComputeShader _writerShader;
+    [SerializeField, HideInInspector] Shader _readerShader;
+
+    Material _readerMaterial;
+    RenderTexture _buffer;
+
+    void Start()
+    {
+        _readerMaterial = new Material(_readerShader);
+
+        _buffer = new RenderTexture(8, 8, 0, RenderTextureFormat.RHalf);
+        _buffer.enableRandomWrite = true;
+        _buffer.dimension = TextureDimension.Tex2DArray;
+        _buffer.volumeDepth = 8;
+        _buffer.Create();
+    }
+
+    void OnDestroy()
+    {
+        if (_readerMaterial != null) Destroy(_readerMaterial);
+        if (_buffer != null) Destroy(_buffer);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        var kernel = _writerShader.FindKernel("WriterMain");
+        _writerShader.SetTexture(kernel, "BufferTex", _buffer);
+        _writerShader.Dispatch(kernel, 1, 1, 1);
+
+        _readerMaterial.SetTexture("_BufferTex", _buffer);
+        Graphics.Blit(null, destination, _readerMaterial, 0);
+    }
+}
